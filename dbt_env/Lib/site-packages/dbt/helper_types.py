6cac0b264414f7668fff17beaf7bc47e8c6f1419
Sia <mbatia@unlv.nevada.edu>
2022-02-22 14:13:04 -0500

    update requirements

---
+++ dbt_env/Lib/site-packages/dbt/helper_types.py
@@ -0,0 +1,102 @@
+# never name this package "types", or mypy will crash in ugly ways
+from dataclasses import dataclass
+from datetime import timedelta
+from pathlib import Path
+from typing import Tuple, AbstractSet, Union
+
+from dbt.dataclass_schema import (
+    dbtClassMixin, ValidationError, StrEnum,
+)
+from hologram import FieldEncoder, JsonDict
+from mashumaro.types import SerializableType
+
+
+class Port(int, SerializableType):
+    @classmethod
+    def _deserialize(cls, value: Union[int, str]) -> 'Port':
+        try:
+            value = int(value)
+        except ValueError:
+            raise ValidationError(f'Cannot encode {value} into port number')
+
+        return Port(value)
+
+    def _serialize(self) -> int:
+        return self
+
+
+class PortEncoder(FieldEncoder):
+    @property
+    def json_schema(self):
+        return {'type': 'integer', 'minimum': 0, 'maximum': 65535}
+
+
+class TimeDeltaFieldEncoder(FieldEncoder[timedelta]):
+    """Encodes timedeltas to dictionaries"""
+
+    def to_wire(self, value: timedelta) -> float:
+        return value.total_seconds()
+
+    def to_python(self, value) -> timedelta:
+        if isinstance(value, timedelta):
+            return value
+        try:
+            return timedelta(seconds=value)
+        except TypeError:
+            raise ValidationError(
+                'cannot encode {} into timedelta'.format(value)
+            ) from None
+
+    @property
+    def json_schema(self) -> JsonDict:
+        return {'type': 'number'}
+
+
+class PathEncoder(FieldEncoder):
+    def to_wire(self, value: Path) -> str:
+        return str(value)
+
+    def to_python(self, value) -> Path:
+        if isinstance(value, Path):
+            return value
+        try:
+            return Path(value)
+        except TypeError:
+            raise ValidationError(
+                'cannot encode {} into timedelta'.format(value)
+            ) from None
+
+    @property
+    def json_schema(self) -> JsonDict:
+        return {'type': 'string'}
+
+
+class NVEnum(StrEnum):
+    novalue = 'novalue'
+
+    def __eq__(self, other):
+        return isinstance(other, NVEnum)
+
+
+@dataclass
+class NoValue(dbtClassMixin):
+    """Sometimes, you want a way to say none that isn't None"""
+    novalue: NVEnum = NVEnum.novalue
+
+
+dbtClassMixin.register_field_encoders({
+    Port: PortEncoder(),
+    timedelta: TimeDeltaFieldEncoder(),
+    Path: PathEncoder(),
+})
+
+
+FQNPath = Tuple[str, ...]
+PathSet = AbstractSet[FQNPath]
+
+
+# This class is used in to_target_dict, so that accesses to missing keys
+# will return an empty string instead of Undefined
+class DictDefaultEmptyStr(dict):
+    def __getitem__(self, key):
+        return dict.get(self, key, "")
---
+++ dbt_env/Lib/site-packages/dbt/version.py
@@ -0,0 +1,132 @@
+import importlib
+import importlib.util
+import os
+import glob
+import json
+from typing import Iterator
+
+import requests
+
+import dbt.exceptions
+import dbt.semver
+
+from dbt.ui import green, red, yellow
+from dbt import flags
+
+PYPI_VERSION_URL = 'https://pypi.org/pypi/dbt-core/json'
+
+
+def get_latest_version(version_url: str = PYPI_VERSION_URL):
+    try:
+        resp = requests.get(version_url)
+        data = resp.json()
+        version_string = data['info']['version']
+    except (json.JSONDecodeError, KeyError, requests.RequestException):
+        return None
+
+    return dbt.semver.VersionSpecifier.from_version_string(version_string)
+
+
+def get_installed_version():
+    return dbt.semver.VersionSpecifier.from_version_string(__version__)
+
+
+def get_package_pypi_url(package_name: str) -> str:
+    return f'https://pypi.org/pypi/dbt-{package_name}/json'
+
+
+def get_version_information():
+    flags.USE_COLORS = True if not flags.USE_COLORS else None
+
+    installed = get_installed_version()
+    latest = get_latest_version()
+
+    installed_s = installed.to_version_string(skip_matcher=True)
+    if latest is None:
+        latest_s = 'unknown'
+    else:
+        latest_s = latest.to_version_string(skip_matcher=True)
+
+    version_msg = ("installed version: {}\n"
+                   "   latest version: {}\n\n".format(installed_s, latest_s))
+
+    plugin_version_msg = "Plugins:\n"
+    for plugin_name, version in _get_dbt_plugins_info():
+        plugin_version = dbt.semver.VersionSpecifier.from_version_string(version)
+        latest_plugin_version = get_latest_version(version_url=get_package_pypi_url(plugin_name))
+        plugin_update_msg = ''
+        if installed == plugin_version or (
+            latest_plugin_version and plugin_version == latest_plugin_version
+        ):
+            compatibility_msg = green('Up to date!')
+        else:
+            if latest_plugin_version:
+                if installed.major == plugin_version.major:
+                    compatibility_msg = yellow('Update available!')
+                else:
+                    compatibility_msg = red('Out of date!')
+                plugin_update_msg = (
+                    "  Your version of dbt-{} is out of date! "
+                    "You can find instructions for upgrading here:\n"
+                    "  https://docs.getdbt.com/dbt-cli/install/overview\n\n"
+                ).format(plugin_name)
+            else:
+                compatibility_msg = yellow('No PYPI version available')
+
+        plugin_version_msg += (
+            "  - {}: {} - {}\n"
+            "{}"
+        ).format(plugin_name, version, compatibility_msg, plugin_update_msg)
+
+    if latest is None:
+        return ("{}The latest version of dbt could not be determined!\n"
+                "Make sure that the following URL is accessible:\n{}\n\n{}"
+                .format(version_msg, PYPI_VERSION_URL, plugin_version_msg)
+                )
+
+    if installed == latest:
+        return f"{version_msg}{green('Up to date!')}\n\n{plugin_version_msg}"
+
+    elif installed > latest:
+        return ("{}Your version of dbt is ahead of the latest "
+                "release!\n\n{}".format(version_msg, plugin_version_msg))
+
+    else:
+        return ("{}Your version of dbt is out of date! "
+                "You can find instructions for upgrading here:\n"
+                "https://docs.getdbt.com/docs/installation\n\n{}"
+                .format(version_msg, plugin_version_msg))
+
+
+def _get_adapter_plugin_names() -> Iterator[str]:
+    spec = importlib.util.find_spec('dbt.adapters')
+    # If None, then nothing provides an importable 'dbt.adapters', so we will
+    # not be reporting plugin versions today
+    if spec is None or spec.submodule_search_locations is None:
+        return
+    for adapters_path in spec.submodule_search_locations:
+        version_glob = os.path.join(adapters_path, '*', '__version__.py')
+        for version_path in glob.glob(version_glob):
+            # the path is like .../dbt/adapters/{plugin_name}/__version__.py
+            # except it could be \\ on windows!
+            plugin_root, _ = os.path.split(version_path)
+            _, plugin_name = os.path.split(plugin_root)
+            yield plugin_name
+
+
+def _get_dbt_plugins_info():
+    for plugin_name in _get_adapter_plugin_names():
+        if plugin_name == 'core':
+            continue
+        try:
+            mod = importlib.import_module(
+                f'dbt.adapters.{plugin_name}.__version__'
+            )
+        except ImportError:
+            # not an adapter
+            continue
+        yield plugin_name, mod.version
+
+
+__version__ = '1.0.3'
+installed = get_installed_version()
